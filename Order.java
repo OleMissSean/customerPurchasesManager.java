c675a73a25336ee1015b4399929d4ab377df07b6
Sean Staz <seanstaz52@gmail.com>
2014-12-22 20:27:04 -0600

    Update Order.java

--- Order.java
+++ Order.java
@@ -7,48 +7,66 @@
      Honor Code Statement: In keeping with the honor code policies of the University of Mississippi, the School of Engineering, and the Department of Computer and Information Science, I affirm that I have neither given nor received assistance on this programming assignment. This assignment represents my individual, original effort. 
                     ... My Signature is on File. 
 */ 
-
-public class Customer 
-{
-	String name;
-	String creditCard;
-	
-	public Customer()
+public class Order 
+{	
+	int pCount = 0;
+	Purchase[] purchase = new Purchase[5];
+	Customer cust;
+	String date;
+			
+	public Order()
 	{
-		name = "";
-		creditCard = "";
+		date = "";
 	}
 	
-	public Customer (String n, String cc)
+	public Order(String d, String n, String cc)
 	{
-		setName(n);
-		setCC(cc);
+		date = d;
+		cust = new Customer(n,cc);
 	}
-	
-	public void setName(String n)
-	{
-		name = n;
-	}
-	
-	public void setCC(String cc)
-	{
-		creditCard = cc;
-	}
-	
-	public String getName()
-	{
-		return name;
-	}
-	
-	public String getCC()
-	{
-		return creditCard;
-	}
-	
-	public String toString()
-	{
-		String str = "[Name: " + name + "]" + " [Credit Card Number: " + creditCard + "]";	
 		
-		return str;
+	public String getOrder()
+	{
+		return date;
+	}
+	
+	public void addPurchase(String pd, double a)
+	{
+		purchase[pCount] =  new Purchase(pd, a);
+		pCount++;
+	}
+	
+	public void changeCustomer(String n)
+	{
+		cust.setName(n);
+	}
+	
+	public void changePurchase(double a)
+	{
+		purchase[1].setAmount(a);
+	}
+	
+	public void printOrder()
+	{
+		System.out.println("The date of the order is: " + date );
+		System.out.println(cust);
+			
+		for(int i = 0; i < purchase.length; i++ )
+		{
+			System.out.println(purchase[i]);
+		}
+	}
+	
+	public void totalPrint()
+	{
+		double total = 0.0;
+		
+		for (int i = 0; i < purchase.length; i++)
+		{
+			total += purchase[i].getAmount();
+		}
+		
+		System.out.println("The total is: " + total);
+		System.out.println("");
 	}
 }
